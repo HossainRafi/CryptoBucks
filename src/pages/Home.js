c36831b609a9a3c48535034ad75e94dfa30cccba
HossainRafi <hossainrafi71@gmail.com>
2023-07-01 15:37:08 +0600

    create navigation component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
 
 const Home = () => {
   return (
     <main className="w-full h-full flex flex-col first-letter:content-center items-center relative text-white font-nunito">
       <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
       <Logo />
+      <Navigation />
+      
       {/* Outlet component for rendering child components */}
       <Outlet />
     </main>
