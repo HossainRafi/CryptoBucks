22de59c598deb0af356a21f1cb98248907ee1ddd
HossainRafi <hossainrafi71@gmail.com>
2023-07-01 15:34:19 +0600

    logo done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router";
+import Logo from "../components/Logo";
 
 const Home = () => {
   return (
     <main className="w-full h-full flex flex-col first-letter:content-center items-center relative text-white font-nunito">
       <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
-
+      <Logo />
       {/* Outlet component for rendering child components */}
       <Outlet />
     </main>
