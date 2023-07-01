136a4e39c2bed21bcd2e54de4875056033f570f3
HossainRafi <hossainrafi71@gmail.com>
2023-07-01 15:50:03 +0600

    button design

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div>
-      Navigation 
-    </div>
-  )
-}
+    <nav className="w-[40%] mt-16 flex justify-around align-middle border border-cyan rounded-lg">
+      <NavLink
+        to="/"
+        className="w-full text-base text-center font-nunito m-2.5 bg-gray-100 hover:text-cyan active:text-gray-300 border-0 cursor-pointer rounded capitalize font-semibold"
+      >
+        Crypto
+      </NavLink>
+    </nav>
+  );
+};
 
-export default Navigation
+export default Navigation;
