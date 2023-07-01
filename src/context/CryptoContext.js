bc04a07440be7625eb0ed125179ed31219d65642
HossainRafi <hossainrafi71@gmail.com>
2023-07-01 22:54:49 +0600

    get data

--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -1,14 +1,30 @@
-import { useState } from "react";
-import { createContext } from "react";
+import { createContext, useState, useLayoutEffect } from "react";
 
 // ============= Create context object ============
 export const CryptoContext = createContext({});
 
 // ============= Create provider component =============
 export const CryptoProvider = ({ children }) => {
-  const [test, setTest] = useState("this is test state");
+  const [cryptoData, setCryptoData] = useState();
 
-  return (
-    <CryptoContext.Provider value={{ test }}>{children}</CryptoContext.Provider>
-  );
+
+  const getCryptoData = async()=>{
+    try{
+        const data = await fetch(
+          `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
+        ).then(res=>res.json()).then(json=>json);
+        console.log(data)
+
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+
+useLayoutEffect(() => {
+  getCryptoData();
+}, []);
+
+
+  return <CryptoContext.Provider value={{}}>{children}</CryptoContext.Provider>;
 };
