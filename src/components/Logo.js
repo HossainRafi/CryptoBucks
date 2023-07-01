e3730e92c6f78ce54b486acec3de66f8edd4fcb1
HossainRafi <hossainrafi71@gmail.com>
2023-07-01 15:59:41 +0600

    navlink component done

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
     <Link
       to="/"
       className="
-     absolute top-[1.5rem] left-[1.5rem] [text-decoration:none] text-lg text-cyan flex items-center"
+     absolute top-[1.5rem] left-[1.5rem] [text-decoration:none] text-xl text-cyan flex items-center"
     >
       <img src={logoSvg} alt="CryptoBucks" />
       <span>CryptoBucks</span>
