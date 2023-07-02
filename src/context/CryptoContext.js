5637dbec719d28a8167fc5bf28e99c556cf30f5e
HossainRafi <hossainrafi71@gmail.com>
2023-07-02 22:06:37 +0600

    modify url

--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -8,12 +8,15 @@ export const CryptoProvider = ({ children }) => {
   // ================ States ====================
   const [cryptoData, setCryptoData] = useState();
   const [searchData, setSearchData] = useState();
+  const [coinSearch, setCoinSearch] = useState("")
+
+
 
   // ================ Get crypto data ===============
   const getCryptoData = async () => {
     try {
       const data = await fetch(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinSearch}&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
       )
         .then((res) => res.json())
         .then((json) => json);
