29656d0498026f67ce5931e65f9392ce652c2c84
HossainRafi <hossainrafi71@gmail.com>
2023-07-02 15:04:43 +0600

    search

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,7 +3,9 @@ import React from "react";
 const Filters = () => {
   return (
     <div className="w-full h-12 border-2 border-gray-100 rounded-lg flex items-center justify-between relative">
-      Filters
+      <div>search</div>
+      <div>currency</div>
+      <div>sorting</div>
     </div>
   );
 };
