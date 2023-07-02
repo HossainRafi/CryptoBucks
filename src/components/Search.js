c6168ed870d33b8eec326dd1a6192b37d1d2242a
HossainRafi <hossainrafi71@gmail.com>
2023-07-02 17:02:08 +0600

    fragement

--- src/components/Search.js
+++ src/components/Search.js
@@ -11,19 +11,34 @@ const Search = () => {
   };
 
   return (
-    <form className="w-96 relative flex items-center ml-7 font-nunito">
-      <input
-        className="w-full rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border border-transparent focus:border-cyan"
-        placeholder="Search Here ....."
-        type="text"
-        name="search"
-        onChange={handleInput}
-        value={searchText}
-      />
-      <button className="absolute right-1 cursor-pointer" type="submit">
-        <img src={searchIcon} alt="icon" className="w-full h-auto" />
-      </button>
-    </form>
+    <>
+      <form className="w-96 relative flex items-center ml-7 font-nunito">
+        <input
+          className="w-full rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border border-transparent focus:border-cyan"
+          placeholder="Search Here ....."
+          type="text"
+          name="search"
+          onChange={handleInput}
+          value={searchText}
+        />
+        <button className="absolute right-1 cursor-pointer" type="submit">
+          <img src={searchIcon} alt="icon" className="w-full h-auto" />
+        </button>
+      </form>
+
+      {
+        searchText.length>0 ? 
+
+        <ul>
+          <li>bitcoin</li>
+          <li>ethereum</li>
+        </ul>
+        :
+
+
+        null
+      }
+    </>
   );
 };
 
