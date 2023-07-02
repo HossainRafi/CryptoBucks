c1aa753df808dc65cc75ec74fd53aa1dc8e7f655
HossainRafi <hossainrafi71@gmail.com>
2023-07-02 21:46:03 +0600

    show search data

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,11 +3,12 @@ import searchIcon from "../assets/search-icon.svg";
 import { CryptoContext } from "../context/CryptoContext";
 import debounce from "lodash.debounce";
 
-
-const SearchInput=({handleSearch})=>{
+const SearchInput = ({ handleSearch }) => {
   const [searchText, setSearchText] = useState("");
+  let {searchData}=useContext(CryptoContext)
 
-   let handleInput = (e) => {
+  // function for input handling
+  let handleInput = (e) => {
     e.preventDefault();
     let query = e.target.value;
     setSearchText(query);
@@ -32,23 +33,21 @@ const SearchInput=({handleSearch})=>{
 
       {searchText.length > 0 ? (
         <ul className="absolute top-11 right-0 w-full h-96 rounded overflow-x-hidden py-2 bg-gray-200 bg-opacity-60 backdrop-blur-md">
-          <li>bitcoin</li>
-          <li>ethereum</li>
+          {searchData ? searchData.map(coin =>{
+            return <li>{coin.id}</li>
+          }) : <h2>Please wait....</h2>}
         </ul>
       ) : null}
     </>
   );
-}
+};
 
 const Search = () => {
-  
   let { getSearchResult } = useContext(CryptoContext);
 
   const debounceFunc = debounce(function (val) {
     getSearchResult(val);
   }, 2000);
-
- 
 
   return (
     <>
