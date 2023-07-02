6c79f4b2ccf6116272df7eb0fc6039b0ebafc602
HossainRafi <hossainrafi71@gmail.com>
2023-07-02 08:33:24 +0600

    change design based on positive or negative

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -56,17 +56,23 @@ const TableComponent = () => {
                   <td className="py-4">{data.name}</td>
                   <td className="py-4">{data.current_price}</td>
                   <td className="py-4">{data.total_volume}</td>
-                  <td className="py-4">
+                  <td className={data.market_cap_change_percentage_24h >0 ?"text-green py-4" : "text-red py-4"}>
                     {Number(data.market_cap_change_percentage_24h).toFixed(3)}
                   </td>
                   <td className="py-4">
-                    {Number(data.price_change_percentage_1h_in_currency).toFixed(3)}
+                    {Number(
+                      data.price_change_percentage_1h_in_currency
+                    ).toFixed(3)}
                   </td>
                   <td className="py-4">
-                    {Number(data.price_change_percentage_24h_in_currency).toFixed(3)}
+                    {Number(
+                      data.price_change_percentage_24h_in_currency
+                    ).toFixed(3)}
                   </td>
                   <td className="py-4">
-                    {Number(data.price_change_percentage_7d_in_currency).toFixed(3)}
+                    {Number(
+                      data.price_change_percentage_7d_in_currency
+                    ).toFixed(3)}
                   </td>
                 </tr>
               );
