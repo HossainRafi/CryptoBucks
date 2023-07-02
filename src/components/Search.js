cec7b68c79e4419c74ba55ac8a3b593402948a7a
HossainRafi <hossainrafi71@gmail.com>
2023-07-02 15:15:55 +0600

    design search

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,9 +3,15 @@ import searchIcon from "../assets/search-icon.svg";
 
 const Search = () => {
   return (
-    <form>
-      <input type="text" name="search" id="" />
-      <button type="submit">
+    <form className="w-96 relative flex items-center ml-7 font-nunito">
+      <input
+        className="w-full rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border border-transparent focus:border-cyan"
+        placeholder="Search Here ....."
+        type="text"
+        name="search"
+        id=""
+      />
+      <button className="absolute right-1 cursor-pointer" type="submit">
         <img src={searchIcon} alt="" />
       </button>
     </form>
