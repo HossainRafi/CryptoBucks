31cd1db89beaa3621fcacc0543287012208f260d
HossainRafi <hossainrafi71@gmail.com>
2023-07-03 20:55:29 +0600

    pagination body

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import paginationArrow from "../assets/pagination-arrow.svg";
 
 const Pagination = () => {
   return (
-    <div>
-      pagination 
+    <div className="flex items-center">
+      <ul>
+        <li>
+          <img className="w-full h-auto" src={paginationArrow} alt="" />
+        </li>
+        <li>...</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>...</li>
+        <li>last-page</li>
+        <li>
+          <img className="w-full h-auto" src={paginationArrow} alt="" />
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
