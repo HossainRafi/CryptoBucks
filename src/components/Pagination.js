5070e5db87c61cab65b6160c2410886e99a7eaae
HossainRafi <hossainrafi71@gmail.com>
2023-07-03 21:12:02 +0600

    pagination design

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -4,18 +4,50 @@ import paginationArrow from "../assets/pagination-arrow.svg";
 const Pagination = () => {
   return (
     <div className="flex items-center">
-      <ul>
-        <li>
-          <img className="w-full h-auto" src={paginationArrow} alt="" />
+      <ul className="flex items-center justify-end text-sm">
+        <li className="flex items-center">
+          <button className="outline-0 hover:text-cyan w-6">
+            <img
+              className="w-full h-auto rotate-180"
+              src={paginationArrow}
+              alt=""
+            />
+          </button>
         </li>
-        <li>...</li>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>...</li>
-        <li>last-page</li>
         <li>
-          <img className="w-full h-auto" src={paginationArrow} alt="" />
+          <button className="outline-0 hover:text-cyan rounded w-6 h-6 flex items-center justify-center text-lg">
+            ...
+          </button>
+        </li>
+        <li>
+          <button className="outline-0 hover:text-cyan rounded-full w-6 h-6 flex items-center justify-center bg-gray-200 mx-1.5">
+            1
+          </button>
+        </li>
+        <li>
+          <button className="outline-0 hover:text-cyan rounded-full w-6 h-6 flex items-center justify-center bg-gray-200 mx-1.5">
+            2
+          </button>
+        </li>
+        <li>
+          <button className="outline-0 hover:text-cyan rounded-full w-6 h-6 flex items-center justify-center bg-gray-200 mx-1.5">
+            3
+          </button>
+        </li>
+        <li>
+          <button className="ouline-0 hover:text-cyan  rounded-full w-6 h-6 flex items-center justify-center text-lg">
+            ...
+          </button>
+        </li>
+        <li>
+          <button className="ouline-0 hover:text-cyan  rounded-full w-6 h-6 flex items-center justify-center bg-gray-200 mx-1.5">
+            100
+          </button>
+        </li>
+        <li className="flex items-center">
+          <button className="outline-0 hover:text-cyan w-6">
+            <img className="w-full h-auto" src={paginationArrow} alt="" />
+          </button>
         </li>
       </ul>
     </div>
