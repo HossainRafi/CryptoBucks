d452c182c1719ace357a86752bd4260bfb844b62
HossainRafi <hossainrafi71@gmail.com>
2023-07-03 16:27:32 +0600

    sorting design

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -43,20 +43,23 @@ const Filters = () => {
             <img src={submitIcon} alt="submit" className="w-full h-auto" />
           </button>
         </form>
+        <label className="relative flex justify-center items-center">
+          <span className="font-bold mr-2">sort by: </span>
+          <select
+            name="sortby"
+            className="rounded bg-gray-200 text-base pl-2 pr-10 py-0.5 leading-4 capitalize focus:outline-0"
+          >
+            <option value="market_cap_desc">market cap desc</option>
+            <option value="market_cap_asc">market cap asc</option>
+            <option value="volume_desc">volume desc</option>
+            <option value="volume_asc">volume asc</option>
+            <option value="id_desc">id desc</option>
+            <option value="id_asc">id asc</option>
+            <option value="gecko_desc">gecko desc</option>
+            <option value="gecko_asc">gecko asc</option>
+          </select>
+        </label>
       </div>
-      <label>
-        <span>sort by:</span>
-        <select name="sortby">
-          <option value="market_cap_desc">market cap desc</option>
-          <option value="market_cap_asc">market cap asc</option>
-          <option value="volume_desc">volume desc</option>
-          <option value="volume_asc">volume asc</option>
-          <option value="id_desc">id desc</option>
-          <option value="id_asc">id asc</option>
-          <option value="gecko_desc">gecko desc</option>
-          <option value="gecko_asc">gecko asc</option>
-        </select>
-      </label>
     </div>
   );
 };
