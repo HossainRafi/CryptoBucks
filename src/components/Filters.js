d517e8b29421673f8bc33b1e331dc961f5baf53e
HossainRafi <hossainrafi71@gmail.com>
2023-07-03 08:48:49 +0600

    form design

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,9 +5,19 @@ const Filters = () => {
   return (
     <div className="w-full h-12 border-2 border-gray-100 rounded-lg flex items-center justify-between relative">
       <Search />
-      <div>
-        <form>
-          <input type="text" />
+      <div className="flex mr-7">
+        <form className="relative flex items-center font-nunito mr-12">
+          <label
+            htmlFor="currency"
+            className="relative flex justify-center items-center mr-2 font-bold"
+          >
+            currency
+          </label>
+          <input
+            type="text"
+            name="currency"
+            placeholder="usd"
+            className="w-16 rounded bg-gray-200 placeholder:text-gray-100 pl-2 required outline-0 border border-transparent focus:border-cyan leading-4"/>
           <button type="submit">submit</button>
         </form>
       </div>
