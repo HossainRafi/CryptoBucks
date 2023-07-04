15ec595098581d726d21d0a91fdf71523188c105
HossainRafi <hossainrafi71@gmail.com>
2023-07-04 21:21:16 +0600

    coin details field design

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -4,7 +4,9 @@ import ReactDOM from "react-dom";
 const CryptoDetails = () => {
   return ReactDOM.createPortal(
     <div className="fixed top-0 w-full h-full bg-gray-200 bg-opacity-30 first-letter: backdrop-blur-sm flex items-center justify-center font-nunito">
-      CryptoDetails
+      <div className="w-[65%] h-[75%] bg-gray-300 bg-opacity-75 rounded-lg text-white relative">
+        CryptoDetails
+      </div>
     </div>,
     document.getElementById("model")
   );
