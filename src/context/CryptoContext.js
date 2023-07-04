24fe3f2e4a5812029cd99eedcd6f7cea60e268fb
HossainRafi <hossainrafi71@gmail.com>
2023-07-04 13:57:31 +0600

    change query in url

--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -17,7 +17,7 @@ export const CryptoProvider = ({ children }) => {
   const getCryptoData = async () => {
     try {
       const data = await fetch(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&ids=${coinSearch}&order=${sortBy}&per_page=10&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
       )
         .then((res) => res.json())
         .then((json) => json);
