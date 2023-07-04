0c6fa7fc61ed1225e00e62ffc311e6b670db2907
HossainRafi <hossainrafi71@gmail.com>
2023-07-04 22:38:10 +0600

    issue solved

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -24,7 +24,10 @@ const CryptoDetails = () => {
       onClick={close}
       className="fixed top-0 w-full h-full bg-gray-200 bg-opacity-30 first-letter: backdrop-blur-sm flex items-center justify-center font-nunito"
     >
-      <div className="w-[65%] h-[75%] bg-gray-300 bg-opacity-75 rounded-lg text-white relative">
+      <div
+        className="w-[65%] h-[75%] bg-gray-300 bg-opacity-75 rounded-lg text-white relative"
+        onClick={(e) => e.stopPropagation()}
+      >
         {coinData ? <h1>{coinData.id}</h1> : null}
       </div>
     </div>,
