5ae60fe92ce2345bde0ed9272adb551f95edc991
HossainRafi <hossainrafi71@gmail.com>
2023-07-05 23:09:03 +0600

    coin social links done

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -196,13 +196,94 @@ const CryptoDetails = () => {
               </div>
 
               <div className="flex w-full mt-4 justify-between">
-                <div className="flx flex-col">
-                  <a href="#">1</a>
-                  <a href="#">2</a>
-                  <a href="#">3</a>
+                <div className="flex flex-col">
+                  <a
+                    target={"_blank"}
+                    rel="noreferrer"
+                    className="text-sm bg-gray-200 text-gray-100 px-1.5 py-0.5 my-1 rounded"
+                    href={data?.links?.homepage[0]}
+                  >
+                    {data?.links?.homepage[0].substring(0, 30)}
+                  </a>
+                  <a
+                    target={"_blank"}
+                    rel="noreferrer"
+                    className="text-sm bg-gray-200 text-gray-100 px-1.5 py-0.5 my-1 rounded"
+                    href={data?.links?.blockchain_site[0]}
+                  >
+                    {data?.links?.blockchain_site[0].substring(0, 30)}
+                  </a>
+
+                  {data?.links?.official_forum_url[0] && (
+                    <a
+                      target={"_blank"}
+                      rel="noreferrer"
+                      className="text-sm bg-gray-200 text-gray-100 px-1.5 py-0.5 my-1 rounded"
+                      href={data?.links?.official_forum_url[0]}
+                    >
+                      {data?.links?.official_forum_url[0].substring(0, 30)}
+                    </a>
+                  )}
                 </div>
+
                 <div className="flex flex-col content-start">
-                  <span>sentiment</span>
+                  <span className="text-sm capitalize text-gray-100">
+                    sentiment
+                  </span>
+                  <div className="flex justify-between">
+                    <div
+                      className={`text-sm px-1 ml-2 my-1 font-medium flex items-center
+          rounded uppercase bg-opacity-25 bg-green text-green
+          
+          `}
+                    >
+                      <span>
+                        {Number(data.sentiment_votes_up_percentage).toFixed(2)}%
+                      </span>
+                      <svg
+                        width="14"
+                        height="14"
+                        viewBox="0 0 14 14"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={`
+                        w-[1rem] ml-0.5
+                        fill-green rotate-180
+                        `}
+                      >
+                        <path d="M7.47951 11.4153C7.42599 11.493 7.35438 11.5565 7.27085 11.6004C7.18732 11.6444 7.09437 11.6673 7.00001 11.6673C6.90564 11.6673 6.81269 11.6444 6.72916 11.6004C6.64563 11.5565 6.57402 11.493 6.52051 11.4153L1.27051 3.83194C1.20974 3.74447 1.1741 3.64202 1.16747 3.53572C1.16084 3.42943 1.18346 3.32334 1.23289 3.229C1.28232 3.13466 1.35665 3.05567 1.44782 3.0006C1.53899 2.94554 1.6435 2.91652 1.75001 2.91669H12.25C12.3563 2.91713 12.4604 2.94652 12.5512 3.00172C12.642 3.05691 12.716 3.13581 12.7653 3.22993C12.8147 3.32406 12.8374 3.42984 12.8311 3.53591C12.8247 3.64199 12.7896 3.74433 12.7295 3.83194L7.47951 11.4153Z" />
+                      </svg>
+                    </div>
+                  </div>
+
+                  <div className="flex justify-between">
+                    <div
+                      className={`text-sm px-1 ml-2 my-1 font-medium flex items-center
+          rounded uppercase bg-opacity-25
+           bg-red text-red
+          `}
+                    >
+                      <span>
+                        {Number(data.sentiment_votes_down_percentage).toFixed(
+                          2
+                        )}
+                        %
+                      </span>
+                      <svg
+                        width="14"
+                        height="14"
+                        viewBox="0 0 14 14"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={`
+                        w-[1rem] ml-0.5
+              "fill-red"
+                        `}
+                      >
+                        <path d="M7.47951 11.4153C7.42599 11.493 7.35438 11.5565 7.27085 11.6004C7.18732 11.6444 7.09437 11.6673 7.00001 11.6673C6.90564 11.6673 6.81269 11.6444 6.72916 11.6004C6.64563 11.5565 6.57402 11.493 6.52051 11.4153L1.27051 3.83194C1.20974 3.74447 1.1741 3.64202 1.16747 3.53572C1.16084 3.42943 1.18346 3.32334 1.23289 3.229C1.28232 3.13466 1.35665 3.05567 1.44782 3.0006C1.53899 2.94554 1.6435 2.91652 1.75001 2.91669H12.25C12.3563 2.91713 12.4604 2.94652 12.5512 3.00172C12.642 3.05691 12.716 3.13581 12.7653 3.22993C12.8147 3.32406 12.8374 3.42984 12.8311 3.53591C12.8247 3.64199 12.7896 3.74433 12.7295 3.83194L7.47951 11.4153Z" />
+                      </svg>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
