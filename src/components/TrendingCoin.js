c3be5f9a951f6794b1bbb606a0f566f08a345bec
HossainRafi <hossainrafi71@gmail.com>
2023-07-07 10:23:12 +0600

    design coin data main div

--- src/components/TrendingCoin.js
+++ src/components/TrendingCoin.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const TrendingCoin = ({data}) => {
+const TrendingCoin = ({ data }) => {
   return (
-    <div>
+    <div className="w-[40%] bg-gray-200 mb-12 last:mb-0 rounded-lg p-4 relative cursor-pointer hover:bg-gray-100 hover:bg-opacity-40">
       {data.item.name}
     </div>
-  )
-}
+  );
+};
 
-export default TrendingCoin
+export default TrendingCoin;
