4205b12570976c8297a72aa0d0b5637e4df65b03
HossainRafi <hossainrafi71@gmail.com>
2023-07-07 10:19:29 +0600

    sending data as prop

---
+++ src/components/TrendingCoin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TrendingCoin = ({data}) => {
+  return (
+    <div>
+      {data.item.name}
+    </div>
+  )
+}
+
+export default TrendingCoin
--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useContext } from "react";
 import { TrendingContext } from "./../context/TrendingContext";
+import TrendingCoin from "../components/TrendingCoin";
 
 const Trending = () => {
   const { trendData } = useContext(TrendingContext);
@@ -8,9 +9,7 @@ const Trending = () => {
   return (
     <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
       <div className="w-full min-h-[60vh] py-8 flex flex-wrap justify-evenly border border-gray-100 rounded">
-        {
-          trendData && trendData.map(coin => <h1>{coin.item.name}</h1>)
-        }
+        {trendData && trendData.map((coin) => <TrendingCoin data={coin} />)}
       </div>
     </section>
   );
