3b6a008dc98bdb712b4dc35f1f9b3d1dedb37786
HossainRafi <hossainrafi71@gmail.com>
2023-07-07 09:36:19 +0600

    trending box field

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -3,7 +3,9 @@ import React from "react";
 const Trending = () => {
   return (
     <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
-      Trending
+      <div className="w-full min-h-[60vh] py-8 flex flex-wrap justify-evenly border border-gray-100 rounded">
+        Trending
+      </div>
     </section>
   );
 };
