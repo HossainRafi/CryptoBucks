0b40d8838e4aae6468a85ec482945b4f09c0c9bd
HossainRafi <hossainrafi71@gmail.com>
2023-07-07 10:25:40 +0600

    display name

--- src/components/TrendingCoin.js
+++ src/components/TrendingCoin.js
@@ -3,7 +3,12 @@ import React from "react";
 const TrendingCoin = ({ data }) => {
   return (
     <div className="w-[40%] bg-gray-200 mb-12 last:mb-0 rounded-lg p-4 relative cursor-pointer hover:bg-gray-100 hover:bg-opacity-40">
-      {data.item.name}
+      {data ? (
+        <h3>
+          <span>name: </span>
+          <span>{data.item.name}</span>
+        </h3>
+      ) : null}
     </div>
   );
 };
