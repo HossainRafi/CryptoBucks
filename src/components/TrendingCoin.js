0c507c7e227ca4aaeb179e6908466eb68f59942d
HossainRafi <hossainrafi71@gmail.com>
2023-07-07 10:33:09 +0600

    implement market cap rank

--- src/components/TrendingCoin.js
+++ src/components/TrendingCoin.js
@@ -4,15 +4,23 @@ const TrendingCoin = ({ data }) => {
   return (
     <div className="w-[40%] bg-gray-200 mb-12 last:mb-0 rounded-lg p-4 relative cursor-pointer hover:bg-gray-100 hover:bg-opacity-40">
       {data ? (
-        <h3 className="text-base flex items-center my-0.5">
-          <span className="text-gray-100 capitalize">name:&nbsp;</span>
-          <span className="text-cyan">{data.name}</span>
-          <img
-            src={data.small}
-            alt={data.name}
-            className="w-[1.5rem] h-[1.5rem] mx-1.5 rounded-full"
-          />
-        </h3>
+        <>
+          <h3 className="text-base flex items-center my-0.5">
+            <span className="text-gray-100 capitalize">name:&nbsp;</span>
+            <span className="text-cyan">{data.name}</span>
+            <img
+              src={data.small}
+              alt={data.name}
+              className="w-[1.5rem] h-[1.5rem] mx-1.5 rounded-full"
+            />
+          </h3>
+          <h3 className="txt-base flex items-center my-0.5">
+            <span className="text-gray-100 capitalize">
+              market cap rank:&nbsp;
+            </span>
+            <span className="text-cyan">{data.market_cap_rank}</span>
+          </h3>
+        </>
       ) : null}
     </div>
   );
