8522fdbd3af8d7588ce5bcaff925f813576ca9b2
HossainRafi <hossainrafi71@gmail.com>
2023-07-07 11:14:49 +0600

    navigation done

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,23 +3,16 @@ import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <nav
-      className="w-[40%] mt-16 flex justify-around align-middle
-    border border-cyan rounded-lg
-    "
-    >
+    <nav className="w-[40%] mt-16 flex justify-around align-middle border border-cyan rounded-lg">
       <NavLink
         to="/"
         end
         className={({ isActive }) => {
-          return `w-full text-base text-center font-nunito m-2.5
-
-${
-  isActive
-    ? "bg-cyan text-gray-300"
-    : "bg-gray-200 text-gray-100 hover:text-cyan active:bg-cyan active:text-gray-300"
-}
-    border-0 cursor-pointer rounded capitalize font-semibold`;
+          return `w-full text-base text-center font-nunito m-2.5 ${
+            isActive
+              ? "bg-cyan text-gray-300"
+              : "bg-gray-200 text-gray-100hover:text-cyan active:bg-cyan active:text-gray-300"
+          } border-0 cursor-pointer rounded capitalize font-semibold`;
         }}
       >
         Crypto
@@ -28,14 +21,11 @@ ${
       <NavLink
         to="/trending"
         className={({ isActive }) => {
-          return `w-full text-base text-center font-nunito m-2.5
-
-${
-  isActive
-    ? "bg-cyan text-gray-300"
-    : "bg-gray-200 text-gray-100 hover:text-cyan active:bg-cyan active:text-gray-300"
-}
-    border-0 cursor-pointer rounded capitalize font-semibold`;
+          return `w-full text-base text-center font-nunito m-2.5 ${
+            isActive
+              ? "bg-cyan text-gray-300"
+              : "bg-gray-200 text-gray-100hover:text-cyan active:bg-cyan active:text-gray-300"
+          } border-0 cursor-pointer rounded capitalize font-semibold`;
         }}
       >
         trending
@@ -44,14 +34,11 @@ ${
       <NavLink
         to="/saved"
         className={({ isActive }) => {
-          return `w-full text-base text-center font-nunito m-2.5
-
-${
-  isActive
-    ? "bg-cyan text-gray-300"
-    : "bg-gray-200 text-gray-100 hover:text-cyan active:bg-cyan active:text-gray-300"
-}
-    border-0 cursor-pointer rounded capitalize font-semibold`;
+          return `w-full text-base text-center font-nunito m-2.5 ${
+            isActive
+              ? "bg-cyan text-gray-300"
+              : "bg-gray-200 text-gray-100hover:text-cyan active:bg-cyan active:text-gray-300"
+          } border-0 cursor-pointer rounded capitalize font-semibold`;
         }}
       >
         saved
