8d259a8b11e736ca419e9e08f917801f5158929e
HossainRafi <hossainrafi71@gmail.com>
2023-07-07 09:40:38 +0600

    get trending coins

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { useContext } from "react";
+import { TrendingContext } from "./../context/TrendingContext";
 
 const Trending = () => {
+  const { trendData } = useContext(TrendingContext);
+
   return (
     <section className="w-[80%] h-full flex flex-col mt-16 mb-24 relative">
       <div className="w-full min-h-[60vh] py-8 flex flex-wrap justify-evenly border border-gray-100 rounded">
-        Trending
+        {
+          trendData && trendData.map(coin => <h1>{coin.item.name}</h1>)
+        }
       </div>
     </section>
   );
